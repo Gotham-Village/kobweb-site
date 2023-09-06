c7678b32758c5434570e38f40e024d1a26f9ce58
David Herman <bitspittle@gmail.com>
2023-09-06 11:03:49 -0700

    Update stale name references to Compose HTML

--- src/jsMain/kotlin/com/varabyte/kobweb/site/components/sections/home/CliSection.kt
+++ src/jsMain/kotlin/com/varabyte/kobweb/site/components/sections/home/CliSection.kt
@@ -32,7 +32,7 @@ fun CliSection() {
             Text("Kobweb CLI")
         }
         SpanText(
-            "Kobweb CLI provides commands to handle the tedious parts of building a Compose for Web app, including project setup and configuration",
+            "Kobweb CLI provides commands to handle the tedious parts of building a Compose HTML app, including project setup and configuration",
             Modifier.lineHeight(1.5).siteText(SiteTextSize.NORMAL).textAlign(TextAlign.Center),
             MutedSpanTextVariant
         )
